865b612052120eb821c18e23c6ce506d75ef9c9a
A <chongya444@gmail.com>
2025-12-27 21:05:07 +0800

    [develop] encrypt uniform

---
+++ utils/uniform/plugin.go
@@ -0,0 +1,17 @@
+package uniform
+
+import (
+	"github.com/zzy-rabbit/patrol/utils/uniform/internal"
+	"github.com/zzy-rabbit/xtools/xcontext"
+	"github.com/zzy-rabbit/xtools/xerror"
+	"github.com/zzy-rabbit/xtools/xplugin"
+)
+
+func init() {
+	ctx := xcontext.Background()
+	plugin := internal.New(ctx)
+	err := xplugin.Register(ctx, plugin)
+	if xerror.Error(err) {
+		panic(err)
+	}
+}
